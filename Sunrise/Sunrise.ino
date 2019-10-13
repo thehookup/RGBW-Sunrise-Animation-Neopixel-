766f3ebf801ea9147913b8d4d0f6a573146a6388
Balázs Bácskay <bacskay.balazs@gmail.com>
2019-10-13 18:54:54 -0400

    Add ESP32 support

--- Sunrise/Sunrise.ino
+++ Sunrise/Sunrise.ino
@@ -1,23 +1,45 @@
+/*****************  USER CONFIG SECTION ***************************************/
+#include "config.h"
+
 /******************  LIBRARY SECTION *************************************/
 #include <Adafruit_NeoPixel.h>    //https://github.com/adafruit/Adafruit_NeoPixel
 #include <SimpleTimer.h>          //https://github.com/thehookup/Simple-Timer-Library
 #include <PubSubClient.h>         //https://github.com/knolleary/pubsubclient
-#include <ESP8266WiFi.h>          //if you get an error here you need to install the ESP8266 board manager 
-#include <ESP8266mDNS.h>          //if you get an error here you need to install the ESP8266 board manager 
-#include <ArduinoOTA.h>           //ArduinoOTA is now included with the ArduinoIDE
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <ESPmDNS.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESP8266mDNS.h>
+#endif
+#ifdef ENABLE_OTA
+  #include <ArduinoOTA.h>           //ArduinoOTA is now included with the ArduinoIDE
+#endif
 
-#include "config.h"
+#ifdef ESP32
+  #define LED_ON HIGH
+  #define LED_OFF LOW
+#else
+  #define LED_ON LOW
+  #define LED_OFF HIGH
+#endif
+
+// Function definitions
+void increaseSunFadeStep();
+void increaseFadeStep();
+void increaseWhiteLevel();
+void increaseSunPhase();
 
 /***********************  WIFI AND MQTT SETUP *****************************/
 /***********************  DON'T CHANGE THIS INFO *****************************/
 
-const char* ssid = USER_SSID ; 
+const char* ssid = USER_SSID ;
 const char* password = USER_PASSWORD ;
 const char* mqtt_server = USER_MQTT_SERVER ;
 const int mqtt_port = USER_MQTT_PORT ;
 const char *mqtt_user = USER_MQTT_USERNAME ;
 const char *mqtt_pass = USER_MQTT_PASSWORD ;
-const char *mqtt_client_name = USER_MQTT_CLIENT_NAME ; 
+const char *mqtt_client_name = USER_MQTT_CLIENT_NAME ;
 
 /*****************  DECLARATIONS  ****************************************/
 WiFiClient espClient;
@@ -48,8 +70,22 @@ int currentSun = 100;
 int oldSun = 0;
 int sunFadeStep = 98;
 
-void setup_wifi() 
+void setup_wifi()
 {
+  #ifdef ESP32
+  WiFi.setSleep(false);
+  #else
+  WiFi.setSleepMode(WIFI_NONE_SLEEP);
+  #endif
+  
+  WiFi.mode(WIFI_STA);
+
+  #ifdef ESP32
+    WiFi.setHostname(USER_MQTT_CLIENT_NAME);
+  #else
+    WiFi.hostname(USER_MQTT_CLIENT_NAME);
+  #endif
+
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
@@ -69,7 +105,7 @@ void setup_wifi()
   Serial.println(WiFi.localIP());
 }
 
-void reconnect() 
+void reconnect()
 {
   // Loop until we're reconnected
   int retries = 0;
@@ -78,7 +114,7 @@ void reconnect()
     {
         Serial.print("Attempting MQTT connection...");
       // Attempt to connect
-      if (client.connect(mqtt_client_name, mqtt_user, mqtt_pass)) 
+      if (client.connect(mqtt_client_name, mqtt_user, mqtt_pass))
       {
         Serial.println("connected");
         // Once connected, publish an announcement...
@@ -89,15 +125,15 @@ void reconnect()
         }
         if(boot == false)
         {
-          client.publish(USER_MQTT_CLIENT_NAME"/checkIn","Reconnected"); 
+          client.publish(USER_MQTT_CLIENT_NAME"/checkIn","Reconnected");
         }
         client.subscribe(USER_MQTT_CLIENT_NAME"/command");
         client.subscribe(USER_MQTT_CLIENT_NAME"/effect");
         client.subscribe(USER_MQTT_CLIENT_NAME"/color");
         client.subscribe(USER_MQTT_CLIENT_NAME"/white");
         client.subscribe(USER_MQTT_CLIENT_NAME"/wakeAlarm");
-      } 
-      else 
+      }
+      else
       {
         Serial.print("failed, rc=");
         Serial.print(client.state());
@@ -121,7 +157,7 @@ void reconnect()
 /************************** MQTT CALLBACK ***********************/
 
 
-void callback(char* topic, byte* payload, unsigned int length) 
+void callback(char* topic, byte* payload, unsigned int length)
 {
   Serial.print("Message arrived [");
   String newTopic = topic;
@@ -161,7 +197,7 @@ void callback(char* topic, byte* payload, unsigned int length)
   }
   if (newTopic == USER_MQTT_CLIENT_NAME "/color")
   {
-    client.publish(USER_MQTT_CLIENT_NAME "/colorState", charPayload); 
+    client.publish(USER_MQTT_CLIENT_NAME "/colorState", charPayload);
     // get the position of the first and second commas
     uint8_t firstIndex = newPayload.indexOf(',');
     uint8_t lastIndex = newPayload.lastIndexOf(',');
@@ -256,7 +292,7 @@ void drawSun()
   {
     sunFadeStep = 0;
   }
-  
+
   int sunStart = (NUM_LEDS/2)-(currentSun/2);
   int newSunLeft = sunStart-1;
   int newSunRight = sunStart+currentSun;
@@ -269,7 +305,7 @@ void drawSun()
   }
   for(int i = sunStart; i < sunStart+currentSun; i++)
   {
-    strip.setPixelColor(i, 255, 64,0,whiteLevel); 
+    strip.setPixelColor(i, 255, 64,0,whiteLevel);
   }
   oldSun = currentSun;
 }
@@ -298,7 +334,7 @@ void drawAurora()
   }
   for(int i = sunStart; i < sunStart+currentAurora; i++)
   {
-    strip.setPixelColor(i, 127, 25,0,0); 
+    strip.setPixelColor(i, 127, 25,0,0);
   }
   oldFadeStep = fadeStep;
   oldAurora = currentAurora;
@@ -308,7 +344,7 @@ void drawAmbient()
 {
   for(int i = 0; i < NUM_LEDS; i++)
   {
-    strip.setPixelColor(i, whiteLevel, 0,0,0); 
+    strip.setPixelColor(i, whiteLevel, 0,0,0);
   }
 }
 
@@ -340,42 +376,53 @@ void selectEffect()
   if(effect == "sunrise")
   {
     sunRise();
+    digitalWrite(LED_BUILTIN, LED_ON);
   }
   if(effect == "mqttRGB")
   {
     mqttRGB();
+    digitalWrite(LED_BUILTIN, LED_ON);
   }
   if(effect == "off")
   {
     off();
+    digitalWrite(LED_BUILTIN, LED_OFF);
   }
 }
 
-void setup() 
+void setup()
 {
+  pinMode(LED_BUILTIN, OUTPUT);          // Initialize the LED_BUILTIN pin as an output
+  digitalWrite(LED_BUILTIN, LED_OFF);    // Turn the LED off by making the voltage HIGH
   Serial.begin(115200);
-  WiFi.setSleepMode(WIFI_NONE_SLEEP);
-  WiFi.mode(WIFI_STA);
+#if defined(DEBUG) && defined(ESP8266)
+  	gdbstub_init();
+#endif
   setup_wifi();
+
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
+  #ifdef ENABLE_OTA
   ArduinoOTA.setHostname(USER_MQTT_CLIENT_NAME);
-  ArduinoOTA.begin(); 
+  ArduinoOTA.begin();
+  #endif
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
 }
 
 void loop() 
 {
-  if (!client.connected()) 
+  if (!client.connected())
   {
     reconnect();
   }
   client.loop();
+  #ifdef ENABLE_OTA
   ArduinoOTA.handle();
+  #endif
   timer.run();
   selectEffect();
-  strip.show(); 
+  strip.show();
 }
 
 

4909b0f653244c4c681b443401b4dbf5c5eb4079
Balázs Bácskay <bacskay.balazs@gmail.com>
2019-10-13 18:35:24 -0400

    Moving the user configuration into config.h

---
+++ Sunrise/config_template.h
@@ -0,0 +1,31 @@
+/*****************  START USER CONFIG SECTION *********************************/
+/*****************  START USER CONFIG SECTION *********************************/
+/*****************  START USER CONFIG SECTION *********************************/
+/*****************  START USER CONFIG SECTION *********************************/
+
+#define USER_SSID                 "YOUR_WIFI_SSID"
+#define USER_PASSWORD             "YOUR_WIFI_PASSWORD"
+#define USER_MQTT_SERVER          "YOUR MQTT SERVER"
+#define USER_MQTT_PORT            1883
+#define USER_MQTT_USERNAME        "YOUR_MQTT_LOGIN"
+#define USER_MQTT_PASSWORD        "YOUR_MQTT_PASSWORD"
+#define USER_MQTT_CLIENT_NAME     "SunriseMCU"           //used to define MQTT topics, MQTT Client ID, and ArduinoOTA
+#if defined(ESP32)
+    #define LED_PIN 23                                   //pin where the led strip is hooked up
+#else
+    #define LED_PIN 5                                    //pin where the led strip is hooked up
+#endif
+#define NUM_LEDS 150                                     //number of LEDs in the strip
+#define BRIGHTNESS 255                                   //strip brightness 255 max
+#define SUNSIZE 30                                       //percentage of the strip that is the "sun"
+
+
+#define ENABLE_OTA 1
+
+//#define DEBUG 1
+
+/*****************  END USER CONFIG SECTION *********************************/
+/*****************  END USER CONFIG SECTION *********************************/
+/*****************  END USER CONFIG SECTION *********************************/
+/*****************  END USER CONFIG SECTION *********************************/
+/*****************  END USER CONFIG SECTION *********************************/
